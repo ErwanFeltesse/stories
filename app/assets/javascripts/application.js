d6fcf0d61b213eed30019b9ad623284dd7bb712b
codecademy <ccuser@codecademy.com>
2024-10-22 09:58:22 +0000

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+        /* JavaScript to open the navigation menu */
+        function openNav() {
+            document.getElementById("navMenu").style.width = "250px";
+        }
+
+        /* JavaScript to close the navigation menu */
+        function closeNav() {
+            document.getElementById("navMenu").style.width = "0";
+        }
